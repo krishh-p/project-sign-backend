bdd4bb26ed2a646e7ed02f716a087799f1aaad67
Krish Patel <krishp123@hotmail.com>
2024-08-17 16:23:42 -0400

    changed prompt

--- app/main.py
+++ app/main.py
@@ -9,10 +9,10 @@ openai_client = OpenAIClient()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
