6bdc96287cfab1bb0fff73cb5a4b2a3a44156abf
Krish Patel <krishp123@hotmail.com>
2024-08-17 16:16:08 -0400

    adding CORS configs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from dto.Prompt import Prompt
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from OpenAIClient import OpenAIClient
 
 app = FastAPI()
 openai_client = OpenAIClient()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def hello():
